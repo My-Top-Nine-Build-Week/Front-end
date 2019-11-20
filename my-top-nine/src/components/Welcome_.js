1f11fa04d92ee0de6b2e6b5a08891b5fa636d085
mjedwards <jairedwards20@gmail.com>
2019-11-20 12:23:49 -0500

    added more styling and animation to Welcome page, also added a new folder that contains a custom font that is used on the welcome page.

--- my-top-nine/src/components/Welcome_.js
+++ my-top-nine/src/components/Welcome_.js
@@ -15,7 +15,61 @@ const WelcomeWrapper = styled.div`
   background-position: center;
 
   h1 {
-    font-size: 4rem;
+    font-size: 3rem;
+    font-family: sans-serif;
+  }
+
+  .tn-display {
+    height: 100vh;
+    width: 100%;
+    text-align: center;
+    font-family: "zilap_monogramaregular";
+  }
+
+  .tn-display ul li {
+    vertical-align: middle;
+    font-size: 80px;
+    letter-spacing: 10px;
+    transition: all ease-in 0.3s;
+    animation: percipitate 6s;
+    margin: 30px 0px;
+  }
+
+  .tn-display ul li:first-child {
+    animation-delay: 0s;
+  }
+
+  .tn-display ul li:nth-child(2) {
+    animation-delay: 0s;
+  }
+
+  .tn-display ul li:nth-child(3) {
+    animation-delay: 0s;
+  }
+
+  .tn-display ul li:nth-child(4) {
+    animation-delay: 0s;
+  }
+
+  .tn-display ul li:nth-child(5) {
+    animation-delay: 0s;
+  }
+
+  .tn-display ul li:nth-child(6) {
+    animation-delay: 0s;
+  }
+
+  @keyframes percipitate {
+    0% {
+      transform: rotate(-90deg) translateX(200px);
+      opacity: 0;
+      filter: blur(20px);
+    }
+    100% {
+      transform: rotate(0deg) translateY(0px);
+      opacity: 1;
+      filter: blur(0px);
+    }
   }
 `;
 
@@ -24,7 +78,17 @@ const Welcome = () => {
     <WelcomeWrapper>
       {/* <img src={require("../images/bg.png")} alt='background image' /> */}
       <div>
-        <h1>My Top Nine</h1>
+        <div class='tn-display'>
+          <ul>
+            <li className='one'>T</li>
+            <li className='two'>O</li>
+            <li className='three'>P</li>
+            <li className='four'>NINE</li>
+            <li className='five'>
+              <h1>Where you can pick and share your favorite things</h1>
+            </li>
+          </ul>
+        </div>
       </div>
     </WelcomeWrapper>
   );
--- my-top-nine/src/index.js
+++ my-top-nine/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./webfontkit1/stylesheet.css";
 
 import "./reset.css";
 import "./index.css";
